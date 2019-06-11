897574368ff38cf2576bedc7c64e04d5c6b951f9
Guuh86 <guh86@outlook.com>
2019-06-11 02:51:02 -0300

    Detalhes e anuncio do evento finalizados

--- src/app/pages/evento/evento.page.ts
+++ src/app/pages/evento/evento.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { LoadingController, ToastController } from '@ionic/angular';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/interfaces/product';
 import { Subscription } from 'rxjs';
@@ -15,23 +13,24 @@ export class EventoPage implements OnInit {
   private productId: string = null;
   public product: Product = {};
   public products = new Array<Product>();
-  private productsSubscription: Subscription;
+  private productSubscription: Subscription;
 
   constructor(
     private productService: ProductService,
     private activatedRoute: ActivatedRoute
   ) {
+    
+  }
+  
+  ngOnInit() {
     this.productId = this.activatedRoute.snapshot.params['id'];
 
     if (this.productId) this.loadProduct();
   }
-  
-  ngOnInit() {
-  }
 
-  loadProduct(){
-    this.productsSubscription = this.productService.getProducts().subscribe(data => {
-      this.products = data;
+  loadProduct() {
+    this.productSubscription = this.productService.getProduct(this.productId).subscribe(data => {
+      this.product = data;
     });
   }
 }
